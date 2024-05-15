367b326718561ef47151e5543360c53671afcc8a
mriusero <marius.ayrault@outlook.com>
2024-05-15 15:35:41 +0200

    Question 1 : OK

--- app_projet_1/src/main/scala/sda/reader/JsonReader.scala
+++ app_projet_1/src/main/scala/sda/reader/JsonReader.scala
@@ -1,5 +1,18 @@
 package sda.reader
 
-class JsonReader {
+import org.apache.spark.sql.{DataFrame, SparkSession}
+case class JsonReader(path: String,
+                     delimiter: Option[String] = None,
+                     header:Option[Boolean] = None
 
+                    )
+  extends Reader {
+  val format = "json"
+
+  def read()(implicit  spark: SparkSession): DataFrame = {
+
+    spark.read.format(format)
+      .option("multiline", true)
+      .load(path)
+  }
 }
